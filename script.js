1d64f7cd172453ecc56314570543e98086adaa29
sjfox <sjfcodes@gmail.com>
2023-02-23 15:54:49 -0800

    display iframes on page load

--- script.js
+++ script.js
@@ -10,22 +10,41 @@ const addRigToList = (e) => {
   if (!validIpRegEx.test(ipAddress)) return;
   if (rigList.map((rig) => rig.ipAddress).includes(ipAddress)) return;
 
-  rigList.push({ location, ipAddress });
+  const newRig = { location, ipAddress };
+
+  rigList.push(newRig);
   setRigList(rigList);
+  displayRig(newRig);
+
   document.getElementById("rig-ip-address").value = "192.168.68.x";
 };
+
 const deleteRigFromList = () => {};
+
 const setRigList = (rigList) => {
   if (!rigList) return;
   localStorage.setItem("rig-list", JSON.stringify(rigList));
 };
+
 const getRigList = () => {
   return JSON.parse(localStorage.getItem("rig-list")) || [];
 };
-const displayRigs = () => {
+
+const displayRig = (rig) => {
+  if (!rig.ipAddress) return;
+  if (!rig.location) return;
+
+  const el = document.createElement("iframe");
+  el.src = 'http://' + rig.ipAddress;
+  el.width = '100%';
+  el.height = '500px'
+  document.body.appendChild(el);
+};
+
+const loadRigs = () => {
   const rigList = getRigList();
-  console.log(rigList);
+  rigList.forEach(displayRig);
 };
 
 addRigFormEl.addEventListener("submit", addRigToList);
-document.addEventListener("DOMContentLoaded", displayRigs);
+document.addEventListener("DOMContentLoaded", loadRigs);
