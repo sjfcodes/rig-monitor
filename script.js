aaf9abbe8643b3ba131bd608bc75a477e7ec78b1
sjfox <sjfcodes@gmail.com>
2023-02-23 16:11:56 -0800

    add feature to delete rig

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const validIpRegEx = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
 const addRigFormEl = document.getElementById("add-rig");
+const dashboardList = document.getElementById("dashboard-list");
 
 const addRigToList = (e) => {
   e.preventDefault();
@@ -19,7 +20,12 @@ const addRigToList = (e) => {
   document.getElementById("rig-ip-address").value = "192.168.68.x";
 };
 
-const deleteRigFromList = () => {};
+const deleteRigFromList = (ipAddress) => {
+  const rigList = getRigList();
+  const updatedList = rigList.filter((rig) => rig.ipAddress !== ipAddress);
+  setRigList(updatedList);
+  loadRigs();
+};
 
 const setRigList = (rigList) => {
   if (!rigList) return;
@@ -34,14 +40,25 @@ const displayRig = (rig) => {
   if (!rig.ipAddress) return;
   if (!rig.location) return;
 
-  const el = document.createElement("iframe");
-  el.src = 'http://' + rig.ipAddress;
-  el.width = '100%';
-  el.height = '500px'
-  document.body.appendChild(el);
+  const iframe = document.createElement("iframe");
+  iframe.src = "http://" + rig.ipAddress;
+  iframe.width = "100%";
+  iframe.height = "500px";
+  dashboardList.appendChild(iframe);
+
+  const deleteEl = document.createElement("button");
+  deleteEl.textContent = "delete " + rig.ipAddress;
+  iframe.classList.add("delete-rig");
+  deleteEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    deleteRigFromList(rig.ipAddress);
+  });
+
+  dashboardList.appendChild(deleteEl);
 };
 
 const loadRigs = () => {
+  dashboardList.innerHTML = "";
   const rigList = getRigList();
   rigList.forEach(displayRig);
 };
